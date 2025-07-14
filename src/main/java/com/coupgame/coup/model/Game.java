e4dbe6feeed35327ca83c5d28a5ce85828c83981
Ameya Soman <somana@eduroam-campus-10-36-134-175.campus-dynamic.uwaterloo.ca>
2025-07-13 22:23:19 -0400

    Game.startGame() completed, deck gets filled with cards (3 of each role) and shuffled, 2 are given to each player in the lobby, the remaining cards become the court deck, and the gameHasStarted boolean gets set to TRUE

--- src/main/java/com/coupgame/coup/model/Game.java
+++ src/main/java/com/coupgame/coup/model/Game.java
@@ -1,12 +1,15 @@
 package com.coupgame.coup.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Game {
 
     private String gameID;
     private ArrayList<Player> players = new ArrayList<>();
     private boolean gameHasStarted = false;
+    private List<CardType> courtDeck = new ArrayList<>();
 
     public Game(String gameID) {
         this.gameID = gameID;
@@ -34,6 +37,46 @@ public class Game {
     }
 
     public void startGame() {
+        List<CardType> deck = new ArrayList<CardType>();
+        for (int i = 0; i < 3; ++i) {
+            for (CardType c : CardType.values()) {
+                deck.add(c);
+            }
+        }
+
+        // ensure the contents of the deck are correct - 3x of each role
+//        for (int i = 0 ; i < deck.size(); ++i) {
+//             System.out.println(deck.get(i));
+//        }
+
+        // shuffle the deck (Fisher-Yates algo variant that runs in O(n))
+        Collections.shuffle(deck);
+
+        // deal each player in the game two cards each
+        for (Player p : this.players) {
+            if (p.getCards().isEmpty()) {
+                // first card
+                p.getCards().add(deck.get(0));
+                deck.remove(0);
+
+                // second card
+                p.getCards().add(deck.get(0));
+                deck.remove(0);
+            }
+        }
+
+        // set remaining cards to be the court deck
+        courtDeck = deck;
+
+        // checks that all player names, their cards, and the court deck is in order
+//        for (Player p : this.players) {
+//            System.out.println("Player: " + p.getName() + ", Cards: " + p.getCards());
+//        }
+//        for (int i = 0; i < courtDeck.size(); ++i) {
+//            System.out.println(courtDeck.get(i));
+//        }
+
+        // game has officially started
         gameHasStarted = true;
     }
 }
